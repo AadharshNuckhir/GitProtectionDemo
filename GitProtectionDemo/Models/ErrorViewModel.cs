25f51ab8e047283197ab60590391fb74dffba951
Aadharsh Nuckhir <aadharshnuckhir@gmail.com>
2023-08-24 16:49:10 +0400

    Initial Commit

---
+++ GitProtectionDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitProtectionDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
